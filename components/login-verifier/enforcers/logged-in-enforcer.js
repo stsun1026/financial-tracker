8432a097982db0867d75629ffeb43a2034580543
Steven Sun <stevensun1026@gmail.com>
2021-03-10 22:56:42 -0700

    Fix jumpy pages
    * Remove login status fetcher and move its functionality to the
    enforcers

--- components/login-verifier/enforcers/logged-in-enforcer.js
+++ components/login-verifier/enforcers/logged-in-enforcer.js
@@ -1,27 +1,49 @@
-import { useEffect } from 'react'
-import { useSelector } from 'react-redux';
+import { useEffect, useLayoutEffect } from 'react'
+import { bindActionCreators } from 'redux'
+import { connect, useSelector } from 'react-redux';
 import Router from 'next/router';
 import { LOGIN_HOME } from '../../../constants/routes';
 import CheckingLoginStatus from '../checking-login-status';
-import LoginStatusFetcher from './login-status-fetcher';
+import {
+  fetchingLoginStatus,
+  fetchingLoginStatusComplete,
+} from '../../../redux/login/actions';
+import firebase from '../../../config/firebase/firebase';
 
 const LoggedInEnforcer = ({children, ...props}) => {
   const isLoggedIn = useSelector(state => state.login.isLoggedIn);
   const isFetchingLoginStatus = useSelector(state => state.login.isFetchingLoginStatus);
-  
+
+  useLayoutEffect(() => {
+    if(isLoggedIn === null) {
+      props.fetchingLoginStatus();
+    }
+  }, []);
+
+
   useEffect(() => {
     if(isFetchingLoginStatus === false && isLoggedIn === false) {
       Router.push(LOGIN_HOME);
     }
   }, [isLoggedIn]);
 
-  const displayChildren = () => !isFetchingLoginStatus && (isLoggedIn || isLoggedIn === null);
-  return (
-    <LoginStatusFetcher>
-      {displayChildren() ? children : <CheckingLoginStatus/>}
-    </LoginStatusFetcher>
-  );
+  if(isFetchingLoginStatus) {
+    firebase.auth().onAuthStateChanged((user) => {
+      props.fetchingLoginStatusComplete(!!user);
+    });
+  }
+
+  if(isFetchingLoginStatus || !isLoggedIn) {
+    return <CheckingLoginStatus/>;
+  }
+  return <div>{children}</div>
 }
 
-export default LoggedInEnforcer;
+const mapDispatchToProps = (dispatch) => {
+  return {
+    fetchingLoginStatus: bindActionCreators(fetchingLoginStatus, dispatch),
+    fetchingLoginStatusComplete: bindActionCreators(fetchingLoginStatusComplete, dispatch),
+  };
+}
 
+export default connect(null, mapDispatchToProps)(LoggedInEnforcer);
--- components/login-verifier/enforcers/logged-out-enforcer.js
+++ components/login-verifier/enforcers/logged-out-enforcer.js
@@ -1,26 +1,47 @@
-import { useEffect } from 'react'
-import { useSelector } from 'react-redux';
+import { useEffect, useLayoutEffect } from 'react'
+import { bindActionCreators } from 'redux'
+import { connect, useSelector } from 'react-redux';
 import Router from 'next/router';
 import { DASHBOARD_HOME } from '../../../constants/routes';
 import CheckingLoginStatus from '../checking-login-status';
-import LoginStatusFetcher from './login-status-fetcher';
+import {
+  fetchingLoginStatus,
+  fetchingLoginStatusComplete,
+} from '../../../redux/login/actions';
+import firebase from '../../../config/firebase/firebase';
 
 const LoggedOutEnforcer = ({children, ...props}) => {
   const isLoggedIn = useSelector(state => state.login.isLoggedIn);
   const isFetchingLoginStatus = useSelector(state => state.login.isFetchingLoginStatus);
   
+  useLayoutEffect(() => {
+    if(isLoggedIn === null) {
+      props.fetchingLoginStatus();
+    }
+  }, []);
+
   useEffect(() => {
     if(isFetchingLoginStatus === false && isLoggedIn === true) {
       Router.push(DASHBOARD_HOME);
     }
   }, [isLoggedIn]);
 
-  const displayChildren = () => !isFetchingLoginStatus && !isLoggedIn;
-  return (
-    <LoginStatusFetcher>
-      {displayChildren() ? children : <CheckingLoginStatus/>}
-    </LoginStatusFetcher>
-    );
+  if(isFetchingLoginStatus) {
+    firebase.auth().onAuthStateChanged((user) => {
+      props.fetchingLoginStatusComplete(!!user);
+    });
+  }
+
+  if(isFetchingLoginStatus || isLoggedIn || isLoggedIn === null) {
+    return <CheckingLoginStatus/>;
+  }
+  return <div>{children}</div>
+}
+const mapDispatchToProps = (dispatch) => {
+  return {
+    fetchingLoginStatus: bindActionCreators(fetchingLoginStatus, dispatch),
+    fetchingLoginStatusComplete: bindActionCreators(fetchingLoginStatusComplete, dispatch),
+  };
 }
 
-export default LoggedOutEnforcer;
+export default connect(null, mapDispatchToProps)(LoggedOutEnforcer);
