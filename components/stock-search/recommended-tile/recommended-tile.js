13cb0aa6f8d2e8a0dfae5a6eace8ab99b8428059
Thisisme-Andrew <andy.tangg4@gmail.com>
2021-04-12 19:03:06 -0600

    Recommended search tiles intial draft

---
+++ components/stock-search/recommended-tile/recommended-tile.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import styles from './recommended-tile.module.css';
+
+const RecommendedTile = ({
+  name, 
+  exchange, 
+  tickerSymbol, 
+  onClick
+}) => {
+  return (  
+    <div className = {styles.mainContainer} onClick = {onClick}>
+      <div className = {styles.innerContainer}>
+        <div className = {styles.nameAndSymbol}>
+          <h1 className = {styles.tickerSymbol}>{tickerSymbol}</h1>
+          <h2 className = {styles.stockName}>{name}</h2>
+        </div>
+        <h2 className = {styles.exchangeName}>{exchange}</h2>
+      </div>
+    </div>
+  )
+};
+
+export default RecommendedTile;
